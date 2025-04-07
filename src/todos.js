e19fbd169abf653dcac280d444ab9a2c2bb0f04c
Corey Gray <16312666+csgray@users.noreply.github.com>
2025-04-07 14:21:00 -0400

    feat: Add ToDos

---
+++ src/todos.js
@@ -0,0 +1,55 @@
+export const Priorities = Object.freeze({
+    High: "High",
+    Medium: "Medium",
+    Low: "Low",
+    None: "None",
+})
+
+export function createToDo(title) {
+    // Title
+    const getTitle = function () {
+        return title;
+    }
+
+    const setTitle = function (newTitle) {
+        title = newTitle;
+    }
+
+    // Description
+    let description;
+
+    const getDescription = function () {
+        return description;
+    }
+
+    const setDescription = function (newDescription) {
+        description = newDescription;
+    }
+
+    // Due Date
+    let dueDate;
+
+    const getDueDate = function () {
+        return dueDate;
+    }
+
+    const setDueDate = function (newDueDate) {
+        dueDate = new Date(newDueDate);
+    }
+
+    // Priority
+    let priority = Priorities.None;
+
+    const getPriority = function () {
+        return priority;
+    }
+
+    const setPriority = function (newPriority) {
+        if (!(newPriority in Priorities)) {
+            throw new Error(`${priority} is not valid. Priority must be one of: ${Priorities.High} ,${Priorities.Medium}, ${Priorities.Low}, ${Priorities.None}`)
+        }
+        priority = Priorities[newPriority];
+    }
+
+    return {getTitle, setTitle, getDescription, setDescription, getDueDate, setDueDate, getPriority, setPriority};
+}
